c8aac60b447722b12fa98a6387b855ee6912664a
ixphinx <riktmp@gmail.com>
2019-11-16 02:38:10 +0000

    desarrollo

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ app.use(require('./routes/index'));
 
 
 //Archivos estaticos
-app.use(express.static(path.join(__dirname + 'public')));
+app.use(express.static(path.join(__dirname + 'src/public')));
+
 
 
 //Run dev
